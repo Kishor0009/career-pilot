72704047e9325e3f212abe4080bd4711cee7f3c4
sakina-cmpn <sakinavi786@gmail.com>
2026-05-29 11:25:39 +0530

    feat: add Telescope Zoom portfolio template and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,6 +29,7 @@ import TextToResume from './pages/TextToResume';
 import About from './components/portfolio/templates/Tech_Startup/About';
 import ChatbotPortfolio from "./components/portfolio/templates/Chatbot_Portfolio";
 import GamifiedXP from "./components/portfolio/templates/Gamified_XP";
+import TelescopeZoom from "./components/portfolio/templates/Telescope_Zoom";
 
 import JobTracker from './pages/JobTracker';
 import { Community, NotFound } from './pages';
@@ -173,6 +174,7 @@ function AppRoutes() {
         <Route path="/templates" element={<TemplateGallery />} />
         <Route path="/templates/chatbot" element={<ChatbotPortfolio />} />
         <Route path="/templates/gamified-xp" element={<GamifiedXP />} />
+        <Route path="/templates/telescope-zoom" element={<TelescopeZoom />} />
         {/* Core Protected Routes */}
         <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="/upload" element={<ProtectedRoute><Upload /></ProtectedRoute>} />
